6082f7c331b315c22bf7a4defb153a378e3da3fd
Drew Code <pekke100@gmail.com>
2023-06-06 06:13:12 -0700

    Producction Ready

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">At Epic Eats, we go beyond just serving delicious food. Our goal is to create memorable dining experiences that captivate all your senses.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Experience our signature dishes, crafted with the finest ingredients and prepared by our talented chefs who are true masters of their craft.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Chef.css';
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">a renowned honor bestowed by Michelin, recognizes exceptional restaurants offering excellent cuisine at affordable prices .</p>
+        </div>
+        <p className="p__opensans"> Ready to embark on a culinary adventure? Stay connected with us by joining the Epic Eats community.  </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
